e75e49d71434ca8ff1eec3d3213db7d1d9a09d77
BrunoAlan <alan.brunoab@gmail.com>
2024-10-29 11:03:17 -0300

    Refactor ActiveLink component to include CSS styles for link and active link

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -6,32 +6,34 @@
 //   });
 // }
 
-import { HomeIcon } from "@primer/octicons-react";
-import Link from "next/link";
+import { HomeIcon } from '@primer/octicons-react';
+import Link from 'next/link';
+import { ActiveLink } from '../active-link/ActiveLink';
 
 const navItems = [
-  { label: 'About', href: '/about' },
-  { label: 'Contact', href: '/contact' },
-  { label: 'Pricing', href: '/pricing' },
-]
+    { label: 'About', href: '/about' },
+    { label: 'Contact', href: '/contact' },
+    { label: 'Pricing', href: '/pricing' },
+];
 
-export const Navbar = async() => {
-  // const text = await temporalAsyncFunction();
+export const Navbar = async () => {
+    // const text = await temporalAsyncFunction();
 
-  return (
-    <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <Link href="/" className="flex items-center gap-2">
-        <HomeIcon size={16} />
-        <span>Home</span>
-      </Link>
-      
-      <div className="flex flex-1"/>
-      {navItems.map((item) => (
-        <Link key={item.label} href = {item.href} className="mr-2">
-          <span>{item.label}</span>
-        </Link>
-      ))}
+    return (
+        <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
+            <Link href='/' className='flex items-center gap-2'>
+                <HomeIcon size={16} />
+                <span>Home</span>
+            </Link>
 
-    </nav>
-  )
-}
+            <div className='flex flex-1' />
+            {navItems.map((item) => (
+                <ActiveLink
+                    key={item.label}
+                    path={item.href}
+                    text={item.label}
+                />
+            ))}
+        </nav>
+    );
+};
