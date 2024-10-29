9324e4927f3e159a64a719709624ca47c9412d8a
BrunoAlan <alan.brunoab@gmail.com>
2024-10-29 09:48:29 -0300

    Refactor Navbar component to include links for About, Contact, and Pricing pages

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -6,16 +6,32 @@
 //   });
 // }
 
+import { HomeIcon } from "@primer/octicons-react";
+import Link from "next/link";
+
+const navItems = [
+  { label: 'About', href: '/about' },
+  { label: 'Contact', href: '/contact' },
+  { label: 'Pricing', href: '/pricing' },
+]
+
 export const Navbar = async() => {
   // const text = await temporalAsyncFunction();
 
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+      <Link href="/" className="flex items-center gap-2">
+        <HomeIcon size={16} />
+        <span>Home</span>
+      </Link>
+      
       <div className="flex flex-1"/>
-      <a className="mr-2" href="/about">About</a>
-      <a className="mr-2" href="/contact">Contact</a>
-      <a className="mr-2" href="/pricing" >Pricing</a>
+      {navItems.map((item) => (
+        <Link key={item.label} href = {item.href} className="mr-2">
+          <span>{item.label}</span>
+        </Link>
+      ))}
+
     </nav>
   )
 }
